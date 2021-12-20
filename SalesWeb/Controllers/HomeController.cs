cf946134f71aae41734182bce3502fd7486642a2
Felipe Passos <felipepassos08@gmail.com>
2021-12-20 16:24:08 -0300

    CRUD Scaffolding

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -4,15 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks; 
+using System.Threading.Tasks;
 
-namespace SalesWeb.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() {
+namespace SalesWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
             return View();
         }
 
-        public IActionResult About() {
+        public IActionResult About()
+        {
             ViewData["Message"] = "Salles Web MVC App";
             ViewData["Autor"] = "Felipe Passos";
             ViewData["email"] = "felipepassos08@email.com";
@@ -21,18 +25,21 @@ namespace SalesWeb.Controllers {
             return View();
         }
 
-        public IActionResult Contact() {
+        public IActionResult Contact()
+        {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
 
-        public IActionResult Privacy() {
+        public IActionResult Privacy()
+        {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
+        public IActionResult Error()
+        {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
